31c2ab7549be1ee42e005bbf408848eb2ba7b027
Ugo Edwin <edwinugochuks28@gmail.com>
2026-09-14 15:05:14 +0100

    Fix exercise one

--- 0x00main.py
+++ 0x00main.py
@@ -1,32 +1,5 @@
-student = {}
-
-print("Student Grading System")
-
-while True:
-    name = input("Enter student name (or type 'done' to finish): ")
-    if name.lower() == 'done':
-        break
-
-    score = int(input(f"Enter {name}'s score: "))
-
-    if score >= 90:
-        grade = 'A'
-    elif score >= 80:
-        grade = 'B'
-    elif score >= 70:
-        grade = 'C'
-    elif score >= 60:
-        grade = 'D'
-    else:
-        grade = 'F'
-
-    student[name] = {"score": score,
-                      "grade": grade
-                      }
-
-    
-print("\n--- Final Report ---")
-
-for name, info in student.items():
-    print(f"{name}: {info['score']} - Grade: {info['grade']}")
-    
+name = "Bob"
+if name == "Bob":
+    print("Hello, Bob!")
+else:
+    print("I don't know you.")
