b76cecf3d9e02883007afeed599664520e7e6cc8
Ugo Edwin <edwinugochuks28@gmail.com>
2026-09-17 13:36:28 +0100

    0x02main.py

---
+++ 0x02main.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+
+print(f"Hi, my name is {name}, I am {age} years old, and I love {hobby}!")
