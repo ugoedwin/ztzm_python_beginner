f4fd29696df0d7b06d2e2234229cac2b1f854872
ugoedwin <edwinugochuks28@gmail.com>
2026-09-22 12:48:16 +0100

    0x05main.py

---
+++ 0x05main.py
@@ -0,0 +1,13 @@
+visited_cities = {"New York", "London", "Tokyo", "Paris", "Sydney"}
+visited_cities.add("Tokyo")
+visited_cities.add("Berlin")
+visited_cities.discard("Sydney")
+
+print("Cities visited")
+for city in visited_cities:
+    print(city)
+
+if "Paris" in visited_cities:
+    print("\nYou've been to Paris.")
+else:
+    print("\nYou haven't been to Paris yet.")
