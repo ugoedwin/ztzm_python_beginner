f1b5fe92f3dfa1a888ce5c540dc5e86c354b5bb3
ugoedwin <edwinugochuks28@gmail.com>
2026-09-23 16:28:00 +0100

    0x09main.py

---
+++ 0x09main.py
@@ -0,0 +1,7 @@
+correct_password = "python123"
+user_input = input("Enter the password: ")
+
+while user_input != correct_password:
+    print("Incorrect password. Try again.")
+    user_input = input("Enter the password:")
+print("Access granted.")
